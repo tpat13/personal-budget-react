73b7b36e01d5598b5303dc5b32e147c2b85a9305
unknown <tpatil@uncc.edu>
2020-10-16 22:14:07 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <div className="center">
+            All rights reserved &copy; Tanvi Patil
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Menu/Menu.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import {
+    Link
+  } from "react-router-dom";
+
+  
+function Menu() {
+  return (
+<nav
+    role = "navigation"
+    aria-label="Main menu"
+    itemScope
+    itemType="https://schema.org/SiteNavigationElement"
+>
+    <div class="menu">
+        <ul>
+            <li><Link itemProp="url" to="/">Home</Link></li>
+            <li><Link itemProp="url" to="/about">About</Link></li>
+            <li><Link itemProp="url" to="/login">Login</Link></li>
+        </ul>
+    </div>
+</nav>
+  );
+}
+
+export default Menu;
